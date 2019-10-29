1ee5289159dd6543d90a2f3f6f7a36488182fb24
Student <example@example.com>
2019-10-29 16:04:34 -0700

    add output messages

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $("form#stress_survey").submit(function(event){
     event.preventDefault();
-    // $("#work-responses").show();
+    $('#stress_survey').hide();
     var stressTotal = 0;
     var stresses = [];
 
@@ -9,27 +9,21 @@ $(document).ready(function(){
       var stressNums = parseInt($(this).val());
       stresses.push(stressNums);
     });
+
     stresses.forEach(function(stress) {
       stressTotal += stress;
     });
+    if(stressTotal < 3) {
+      $('#rec1').show();
+    } else if (stressTotal < 8) {
+      $('#rec2').show();
+    } else if (stressTotal >= 8) {
+      $('#rec3').show();
+    } else {
+      $('#error').show();
+    }
     console.log(stressTotal);
-    //
-    // console.log(stressTotal);
+
+
   });
-
-
-  // $('#transportation_survey').hide();
 });
-//   $("#fun-responses").show();
-//   $("input:checkbox[name=stressSymptoms]:checked").each(function(){
-  //     var stressSym = $(this).val();
-  //     $('#fun-responses').append(stressSymptoms + "<br>");
-  //   });
-  //   $("#fun-responses").show();
-  //
-  //   $("input:checkbox[name=fun-transportation]:checked").each(function(){
-    //     var funTransportationMode = $(this).val();
-    //     $('#fun-responses').append(funTransportationMode + "<br>");
-    //   });
-
-    // $('#work-responses').append(stressNeg + "<br>")
